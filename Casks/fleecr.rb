98721465f446bbae69595fa2858be61efbca31a5
voidyuu <zyuxon@gmail.com>
2026-09-10 13:01:56 +0800

    feat: add fleecr cask

---
+++ Casks/fleecr.rb
@@ -0,0 +1,27 @@
+cask "fleecr" do
+  version "0.5.3"
+  sha256 "27c925cb4ae676b70cebd89e3698546fa377a0cb611acdd097bbf5d309df3967"
+
+  url "https://github.com/voidyuu/fleecr/releases/download/v#{version}/fleecr-#{version}.zip"
+  name "fleecr"
+  desc "Native macOS client for herdr"
+  homepage "https://github.com/voidyuu/fleecr"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Fleecr.app"
+
+  zap trash: [
+    "~/Library/Application Support/Fleecr",
+    "~/Library/Caches/dev.bybee.fleecr",
+    "~/Library/HTTPStorages/dev.bybee.fleecr",
+    "~/Library/Preferences/dev.bybee.fleecr.plist",
+    "~/Library/Saved Application State/dev.bybee.fleecr.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# voidyuu's Homebrew Tap
+
+Official Homebrew tap for [fleecr](https://github.com/voidyuu/fleecr).
+
+## Installation
+
+```sh
+brew install voidyuu/tap/fleecr
+```
+
+Or tap first:
+
+```sh
+brew tap voidyuu/tap
+brew install --cask fleecr
+```
